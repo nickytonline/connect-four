1931ce4aebf87e5a2c40ef30a30f3dc91a8c7350
Nick Taylor <nick@iamdeveloper.com>
2022-09-25 19:35:42 -0400

    chore: added --passWithNoTests in case there are no related tests when running precommit

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,6 +2,14 @@ module.exports = {
 	'*.json': ['prettier --write'],
 	'*.md': ['prettier --write --prose-wrap always'],
 	'*.{css,scss}': ['prettier --write'],
-	'*.ts': ['prettier --write', 'eslint --fix', 'vitest run related'],
-	'*.svelte': ['prettier --write', 'eslint --fix', 'vitest run related'],
+	'*.ts': [
+		'prettier --write',
+		'eslint --fix',
+		'vitest run related --passWithNoTests',
+	],
+	'*.svelte': [
+		'prettier --write',
+		'eslint --fix',
+		'vitest run related --passWithNoTests',
+	],
 };
