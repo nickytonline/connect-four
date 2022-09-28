cee6d585ad8980c7b68c5f1c8de992f7fc2e64ee
Nick Taylor <nick@iamdeveloper.com>
2022-09-28 05:17:57 -0400

    chore: cleaned up lint-staged config (#9)

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,12 +2,7 @@ module.exports = {
 	'*.json': ['prettier --write'],
 	'*.md': ['prettier --write --prose-wrap always'],
 	'*.{css,scss}': ['prettier --write'],
-	'*.ts': [
-		'prettier --write',
-		'eslint --fix',
-		'vitest run related --passWithNoTests',
-	],
-	'*.svelte': [
+	'*.{ts,svelte}': [
 		'prettier --write',
 		'eslint --fix',
 		'vitest run related --passWithNoTests',
