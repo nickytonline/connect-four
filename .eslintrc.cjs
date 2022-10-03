c343ab709f2cf46cc3c9548aad058e6497d74278
Nick Taylor <nick@iamdeveloper.com>
2022-10-03 18:47:37 -0400

    feat: moved to SvelteKit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,20 @@
-// eslint-env node
 module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript')
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
 	env: {
 		browser: true,
-		es2021: true,
-	},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	overrides: [
-		{
-			files: ['*.svelte'],
-			processor: 'svelte3/svelte3',
-		},
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['svelte3', '@typescript-eslint'],
-	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-	},
-	settings: {
-		'svelte3/typescript': true,
-	},
+		es2017: true,
+		node: true
+	}
 };
--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,9 +2,5 @@ module.exports = {
 	'*.json': ['prettier --write'],
 	'*.md': ['prettier --write --prose-wrap always'],
 	'*.{css,scss}': ['prettier --write'],
-	'*.{ts,svelte}': [
-		'prettier --write',
-		'eslint --fix',
-		'vitest run related --passWithNoTests',
-	],
+	'*.{ts,svelte}': ['prettier --write', 'eslint --fix', 'vitest run related --passWithNoTests']
 };
