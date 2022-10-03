7fa6d7655bb734ddee875832a757dd2f9c056074
Nick Taylor <nick@iamdeveloper.com>
2022-10-03 14:06:16 -0400

    chore: wip game pieces

---
+++ src/lib/Board.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { gameState } from '../lib/stores/gameState';
+	import Coin from './Coin.svelte';
+</script>
+
+<div data-board>
+	{#each $gameState.boardState as column, colIndex}
+		<div data-column={colIndex}>
+			{#each column as cell, rowIndex}
+				<div data-cell="{colIndex},{rowIndex}">
+					{#if cell}
+						<Coin player={$gameState.players[cell]} />
+					{:else}
+						{colIndex},{rowIndex}
+					{/if}
+				</div>
+			{/each}
+		</div>
+	{/each}
+</div>
+
+<style>
+	/* Use a PNG for board background and use transparent BG for cells */
+	div[data-board] {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		/* grid-template-rows: repeat(6, 1fr); */
+		grid-gap: 1rem;
+		width: 28rem;
+		height: 25rem;
+		background-color: blue;
+	}
+
+	div[data-board] [data-cell] {
+		width: 3rem;
+		height: 3rem;
+		border: 4px solid white;
+		background-color: white;
+		border-radius: 50%;
+	}
+</style>
---
+++ src/lib/stores/gameState.ts
@@ -0,0 +1,17 @@
+import type { Player } from 'src/types/GameTypes';
+import { writable } from 'svelte/store';
+
+type Players = 0 | 1;
+type CellState = Players | undefined;
+
+interface GameState {
+	players: [Player, Player];
+	playerTurn: Players;
+	boardState: CellState[][];
+}
+
+export const gameState = writable<GameState>({
+	players: [{ color: 'R' }, { color: 'Y' }],
+	playerTurn: 1,
+	boardState: new Array(7).fill(new Array(6)),
+});
