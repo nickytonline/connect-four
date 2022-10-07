5991ab06a198124f585dcf66dc505bf66c34aee6
Nick Taylor <nick@iamdeveloper.com>
2022-10-07 09:22:00 -0400

    feat: moved to SvelteKit (#20)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<header>
+	<h1>The beginnings of the classic Connect Four game.</h1>
+</header>
+<main>
+	<h2>Welcome to SvelteKit</h2>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+Footer © 2022 GitHub, Inc. Footer navigation
+
+<style>
+	h1 {
+		padding: 1rem;
+		border: 2rem dotted blue;
+	}
+</style>
