c481e1a5d8a9eaf98067972859d6402f4e7e65e6
jocrah <ocrahjoshua@gmail.com>
2022-10-08 17:33:58 +0000

    feat: add Coin component

---
+++ src/lib/Coin.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Player } from '../types/GameTypes';
+
+	export let player: Player;
+</script>
+
+<div>{player.color}</div>
+
+<style>
+	div {
+		display: grid;
+		place-items: center;
+		font-size: 3rem;
+		font-weight: bold;
+		border-radius: 50%;
+		border: dotted 4px #000;
+		width: 100px;
+		height: 100px;
+	}
+</style>
